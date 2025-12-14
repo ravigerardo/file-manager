0cad066eb01c897288e84150d075e444366a7ce3
Gerardo <ravishgerardo@gmail.com>
2025-12-14 01:51:24 -0800

    Navegation history

--- main.rb
+++ main.rb
@@ -9,24 +9,59 @@ class FileManager
   HOME_PATH = '~'.freeze
 
   def initialize
+    initial_sate
+    set_rows
+  end
+
+  def initial_sate
     @pwd = HOME_PATH
     @rows = []
-    @history = []
-    set_rows
+    @history = [HOME_PATH]
+    @index_history = 0
   end
 
   def navegate(path)
     self.pwd = path
     set_rows
+    self.history = @history[0..@index_history]
     self.history << path
     self.index_history = @history.size - 1
   end
 
   def navegate_history(is_back: false)
+    return unless can_navegate?(is_back: is_back)
+
     index = is_back ? self.index_history - 1 : self.index_history + 1
+    self.index_history = index
     path = @history[index]
     self.pwd = path
     set_rows
+  end
+
+  def can_navegate?(is_back: false)
+    return false if is_back && @index_history < 1
+    return false if !is_back && @index_history >= @history.size - 1
+
+    true
+  end
+
+  def set_rows
+    rows = []
+    raw_rows = `ls -l #{@pwd}`.split("\n").drop(1)
+    raw_rows.each do |c|
+      raw_columns = c.split
+      rows << Row.new(
+        type: raw_columns[0],
+        links: raw_columns[1],
+        owner: raw_columns[2],
+        group: raw_columns[3],
+        fsize: raw_columns[4],
+        updated_at: "#{raw_columns[5]} #{raw_columns[6]} #{raw_columns[7]}",
+        name: raw_columns[8]
+      )
+    end
+
+    self.rows = rows
   end
 
   def launch
@@ -37,19 +72,12 @@ class FileManager
         horizontal_box {
           stretchy false
 
-          # button('🐛') {
-          #   stretchy false
-          #
-          #   on_clicked do
-          #     puts "History: #{self.history}"
-          #     puts "Index: #{self.index_history}"
-          #   end
-          # }
           button('🏠') {
             stretchy false
 
             on_clicked do
-              navegate HOME_PATH
+              initial_sate
+              set_rows
             end
           }
           button('⬅️') {
@@ -93,25 +121,6 @@ class FileManager
         }
       }
     }.show
-  end
-
-  def set_rows
-    rows = []
-    raw_rows = `ls -l #{@pwd}`.split("\n").drop(1)
-    raw_rows.each do |c|
-      raw_columns = c.split
-      rows << Row.new(
-        type: raw_columns[0],
-        links: raw_columns[1],
-        owner: raw_columns[2],
-        group: raw_columns[3],
-        fsize: raw_columns[4],
-        updated_at: "#{raw_columns[5]} #{raw_columns[6]} #{raw_columns[7]}",
-        name: raw_columns[8]
-      )
-    end
-
-    self.rows = rows
   end
 end
 
